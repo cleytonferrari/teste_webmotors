6347c0ab38c5b60f59bab38dd76a7f0d8b2a372a
cleytonferrari <cleyton@w7br.com>
2021-07-10 11:59:09 -0400

    Criado um repositorio inicial em ADO

---
+++ Teste_WebMotors.RepositorioADO/AnuncioWebmotorsRepositorio.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Teste_WebMotors.Dominio;
+using Teste_WebMotors.Dominio.Contratos;
+
+namespace Teste_WebMotors.RepositorioADO
+{
+    public class AnuncioWebmotorsRepositorio : IAnuncioWebmotors
+    {
+        private readonly string _connString;
+        private Contexto contexto;
+        public AnuncioWebmotorsRepositorio(string connString)
+        {
+            _connString = connString;
+        }
+
+        public int Atualizar(AnuncioWebmotors entidade)
+        {
+            var strQuery = new StringBuilder();
+            strQuery.Append(" UPDATE tb_AnuncioWebmotors SET ");
+            strQuery.Append($" marca = '{entidade.Marca}', ");
+            strQuery.Append($" modelo = '{entidade.Modelo}', ");
+            strQuery.Append($" versao = '{entidade.Versao}', ");
+            strQuery.Append($" ano = '{entidade.Ano}', ");
+            strQuery.Append($" quilometragem = '{entidade.Quilometragem}', ");
+            strQuery.Append($" observacao = '{entidade.Observacao}' ");
+            strQuery.Append($" WHERE ID = {entidade.Id} ");
+            
+            using (contexto = new Contexto(_connString))
+            {
+                return contexto.ExecutaComando(strQuery.ToString());
+            }
+        }
+
+        public List<AnuncioWebmotors> Buscar()
+        {
+            var strQuery = new StringBuilder();
+            strQuery.Append(" SELECT ID, marca, modelo, versao, ano, quilometragem, observacao ");
+            strQuery.Append(" FROM tb_AnuncioWebmotors ");
+            
+            using (contexto = new Contexto(_connString))
+            {
+                var retorno = contexto.ExecutaComandoComRetorno(strQuery.ToString());
+                return TransformaReaderEmListaDeObjeto(retorno);
+            }
+        }
+
+        public AnuncioWebmotors BuscarPorId(int id)
+        {
+            var strQuery = new StringBuilder();
+            strQuery.Append(" SELECT ID, marca, modelo, versao, ano, quilometragem, observacao ");
+            strQuery.Append(" FROM tb_AnuncioWebmotors ");
+            strQuery.Append($" WHERE ID = {id} ");
+            using (contexto = new Contexto(_connString))
+            {
+                var retorno = contexto.ExecutaComandoComRetorno(strQuery.ToString());
+                return TransformaReaderEmListaDeObjeto(retorno).FirstOrDefault();
+            }
+        }
+
+        public int Inserir(AnuncioWebmotors entidade)
+        {
+            var strQuery = new StringBuilder();
+            strQuery.Append(" INSERT INTO tb_AnuncioWebmotors (marca, modelo, versao, ano, quilometragem, observacao) ");
+            strQuery.Append($" VALUES ('{entidade.Marca}','{entidade.Modelo}','{entidade.Versao}','{entidade.Ano}','{entidade.Quilometragem}','{entidade.Observacao}') ");
+
+            using (contexto = new Contexto(_connString))
+            {
+                //TODO: retornar o id do objeto inserido
+                return contexto.ExecutaComando(strQuery.ToString());
+            }
+        }
+
+        public int Remover(int id)
+        {
+            var strQuery = new StringBuilder();
+            strQuery.Append(" DELETE FROM tb_AnuncioWebmotors ");
+            strQuery.Append($" WHERE ID = {id} ");
+
+            using (contexto = new Contexto(_connString))
+            {
+                return contexto.ExecutaComando(strQuery.ToString());
+            }
+        }
+
+        private List<AnuncioWebmotors> TransformaReaderEmListaDeObjeto(List<Dictionary<string, string>> rows)
+        {
+            var anuncio = new List<AnuncioWebmotors>();
+            foreach (var row in rows)
+            {
+                var temObjeto = new AnuncioWebmotors()
+                {
+                    Id = int.Parse(!string.IsNullOrEmpty(row["ID"]) ? row["ID"] : "0"),
+                    Marca = row["marca"],
+                    Modelo = row["modelo"],
+                    Versao = row["versao"],
+                    Ano = int.Parse(!string.IsNullOrEmpty(row["ano"]) ? row["ano"] : "0"),
+                    Quilometragem = int.Parse(!string.IsNullOrEmpty(row["ano"]) ? row["ano"] : "0"),
+                    Observacao = row["observacao"],
+                };
+                anuncio.Add(temObjeto);
+            }
+            return anuncio;
+        }
+    }
+}
---
+++ Teste_WebMotors.RepositorioADO/Contexto.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Teste_WebMotors.RepositorioADO
+{
+    public class Contexto : IDisposable
+    {
+        private SqlConnection _minhaConexao;
+
+        public Contexto(string connString)
+        {
+            _minhaConexao = new SqlConnection(connString);
+        }
+        public int ExecutaComando(string strQuery)
+        {
+            if (String.IsNullOrEmpty(strQuery))
+                throw new ArgumentException("O comandoSQL não pode ser nulo ou vazio");
+
+            try
+            {
+                AbrirConexao();
+                using var cmdComando = CriarComando(strQuery);
+                return cmdComando.ExecuteNonQuery();
+            }
+            finally
+            {
+                FecharConexao();
+            }
+        }
+
+        public List<Dictionary<string, string>> ExecutaComandoComRetorno(string comandoSQL)
+        {
+            List<Dictionary<string, string>> linhas = null;
+
+            if (String.IsNullOrEmpty(comandoSQL))
+            {
+                throw new ArgumentException("O comandoSQL não pode ser nulo ou vazio");
+            }
+            try
+            {
+
+                using (var cmdComando = CriarComando(comandoSQL))
+                {
+                    AbrirConexao();
+                    using (var reader = cmdComando.ExecuteReader())
+                    {
+                        linhas = new List<Dictionary<string, string>>();
+                        while (reader.Read())
+                        {
+                            var linha = new Dictionary<string, string>();
+
+                            for (var i = 0; i < reader.FieldCount; i++)
+                            {
+                                var nomeDaColuna = reader.GetName(i);
+                                var valorDaColuna = reader.IsDBNull(i) ? "" : reader.GetValue(i);// GetString(i);
+
+                                linha.Add(nomeDaColuna, valorDaColuna.ToString());
+                            }
+                            linhas.Add(linha);
+                        }
+                    }
+                }
+
+            }
+            finally
+            {
+                FecharConexao();
+            }
+
+            return linhas;
+        }
+
+
+        private SqlCommand CriarComando(string comandoSQL)
+        {
+            var cmdComando = _minhaConexao.CreateCommand();
+            cmdComando.CommandText = comandoSQL;
+            cmdComando.CommandTimeout = 0;
+            return cmdComando;
+        }
+
+        private void AbrirConexao()
+        {
+            var tentativas = 3;
+
+            if (_minhaConexao.State == ConnectionState.Open)
+            {
+                return;
+            }
+            else
+            {
+                while (tentativas >= 0 && _minhaConexao.State != ConnectionState.Open)
+                {
+                    _minhaConexao.Open();
+                    tentativas--;
+                    Thread.Sleep(30);
+                }
+            }
+        }
+
+        private void FecharConexao()
+        {
+            if (_minhaConexao.State == ConnectionState.Open)
+            {
+                _minhaConexao.Dispose();
+                _minhaConexao.Close();
+            }
+            SqlConnection.ClearAllPools();
+        }
+
+        public void Dispose()
+        {
+            if (_minhaConexao == null) return;
+
+            _minhaConexao.Dispose();
+            _minhaConexao = null;
+            GC.Collect();
+        }
+
+    }
+}
