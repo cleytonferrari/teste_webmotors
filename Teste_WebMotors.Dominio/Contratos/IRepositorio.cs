bc536e18cf6a2d7991f0f30bce5c2ce420071714
cleytonferrari <cleyton@w7br.com>
2021-07-10 11:58:23 -0400

    Atualizado os metodos da interface

--- Teste_WebMotors.Dominio/Contratos/IRepositorio.cs
+++ Teste_WebMotors.Dominio/Contratos/IRepositorio.cs
@@ -1,29 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Teste_WebMotors.Dominio.Contratos
 {
     public interface IRepositorio<T> where T: class
     {
-        Task<T> BuscarPorId(string id);
+        T BuscarPorId(int id);
 
-        IQueryable<T> Buscar();
+        List<T> Buscar();
 
-        Task<IEnumerable<T>> Todos();
+        int Inserir(T entidade);
 
-        Task InserirAsync(T entidade);
+        int Atualizar(T entidade);
 
-        void Inserir(T entidade);
-
-        Task InserirAsync(IEnumerable<T> entidades);
-
-        Task AtualizarAsync(T entidade);
-
-        void Atualizar(T entidade);
-
-        Task Remover(string id);
+        int Remover(int id);
     }
 }
